88b22026505e967296c7485ae9b32da6bf46065c
Van Tran Y <yvantran0506@gmail.com>
2024-09-03 22:25:59 +0700

    done section 8

---
+++ section8/accounts/src/main/java/com/eazybytes/accounts/services/ICustomerService.java
@@ -0,0 +1,13 @@
+package com.eazybytes.accounts.services;
+
+import com.eazybytes.accounts.dto.CustomerDetailsDto;
+
+public interface ICustomerService {
+
+    /**
+     *
+     * @param mobileNumber - Input mobile number
+     * @return Customer Details based on mobile number
+     */
+    CustomerDetailsDto fetchCustomerDetails(String mobileNumber);
+}
---
+++ section8/accounts/src/main/java/com/eazybytes/accounts/services/impl/CustomerServiceImpl.java
@@ -0,0 +1,55 @@
+package com.eazybytes.accounts.services.impl;
+
+import com.eazybytes.accounts.dto.AccountDTO;
+import com.eazybytes.accounts.dto.CardsDto;
+import com.eazybytes.accounts.dto.CustomerDetailsDto;
+import com.eazybytes.accounts.dto.LoanDto;
+import com.eazybytes.accounts.entities.Account;
+import com.eazybytes.accounts.entities.Customer;
+import com.eazybytes.accounts.exceptions.ResourceNotFoundException;
+import com.eazybytes.accounts.mapper.AccountMapper;
+import com.eazybytes.accounts.mapper.CustomerMapper;
+import com.eazybytes.accounts.repositories.AccountRepository;
+import com.eazybytes.accounts.repositories.CustomerRepository;
+import com.eazybytes.accounts.services.ICustomerService;
+import com.eazybytes.accounts.services.client.CardsFeignClient;
+import com.eazybytes.accounts.services.client.LoansFeignClient;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CustomerServiceImpl implements ICustomerService {
+
+    private AccountRepository accountRepository;
+    private CustomerRepository customerRepository;
+    private CardsFeignClient cardsFeignClient;
+    private LoansFeignClient loansFeignClient;
+
+    /**
+     *
+     * @param mobileNumber - Input mobile number
+     * @return Customer Details based on mobile number
+     */
+    @Override
+    public CustomerDetailsDto fetchCustomerDetails(String mobileNumber) {
+        Customer customer = customerRepository.findByMobileNumber(mobileNumber).orElseThrow(
+                () -> new ResourceNotFoundException("Customer", "mobileNumber", mobileNumber)
+        );
+        Account account = accountRepository.findByCustomerId(customer.getCustomerId()).orElseThrow(
+                () -> new ResourceNotFoundException("Account", "customerId", customer.getCustomerId().toString())
+        );
+
+        CustomerDetailsDto customerDetailsDto = CustomerMapper.mapToCustomerDetailsDto(customer, new CustomerDetailsDto());
+        customerDetailsDto.setAccountDTO(AccountMapper.mapToAccountsDto(account, new AccountDTO()));
+
+        ResponseEntity<LoanDto> loanDtoResponseEntity = loansFeignClient.fetchLoanDetail(mobileNumber);
+        customerDetailsDto.setLoansDto(loanDtoResponseEntity.getBody());
+
+        ResponseEntity<CardsDto> cardsDtoResponseEntity = cardsFeignClient.fetchCardDetails(mobileNumber);
+        customerDetailsDto.setCardsDto(cardsDtoResponseEntity.getBody());
+
+        return customerDetailsDto;
+    }
+}
